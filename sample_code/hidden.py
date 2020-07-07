a0f06b50ec8aa7a2174b9e3441e02d230875eaff
sspleo <sspleo@gmail.com>
2020-07-07 18:14:32 +0900

    update

--- sample_code/hidden.py
+++ sample_code/hidden.py
@@ -3,8 +3,9 @@
 # https://apps.twitter.com/
 # Create new App and get the four strings
 
+
 def oauth():
-    return {"consumer_key": "h7Lu...Ng",
-            "consumer_secret": "dNKenAC3New...mmn7Q",
-            "token_key": "10185562-eibxCp9n2...P4GEQQOSGI",
-            "token_secret": "H0ycCFemmC4wyf1...qoIpBo"}
+    return {"consumer_key": "MLGXtuMd38C2SJqIsFoSV5Z87",
+            "consumer_secret": "qIecCNsG6Dq4I0veED7hkuV2BseBkkPz91uhjin2xOR0J7vxk1",
+            "token_key": "2447428062-odtqX630f2mwfJovyfPi7cQXb35eI3z3RVoYtJD",
+            "token_secret": "RK1XwkLXxNY79hqn6H5KKobtgC2weFpxbzNUPFgVErFs7"}
